392312d27f5d0d33911eccbd75bfa98818dbe2f1
bochan7 <155892265+bochan7@users.noreply.github.com>
2024-03-03 15:28:23 +0530

    weekend commit

--- ppr_ques/union.c
+++ ppr_ques/union.c
@@ -22,19 +22,3 @@ void printUnion(int arr1[],int arr2[],int m,int n){
         printf("%d",arr1[i]);
         i++;
     }
-
-   while(j< n){
-    printf("%d",arr2[j]);
-    j++;
-   }
-}
-int main(){
-    int arr1[] = {1,3,4,5,6};
-    int arr2[] = {3,4,5,6,8,9,12,12};
-    int m,n;
-    m = sizeof(arr1)/sizeof(arr1[0]);
-    n = sizeof(arr2)/sizeof(arr2[0]);
-    printf("the union of the following array:\n");
-    printUnion(arr1,arr2,m,n);
-return 0;
-}
